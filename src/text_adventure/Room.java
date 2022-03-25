3b730c879de9ae30e0373227fea5d4138308147b
KuzeyOzturac <kuzeyozturac2@gmail.com>
2022-03-25 09:48:59 -0400

    finished the rooms

--- src/text_adventure/Room.java
+++ src/text_adventure/Room.java
@@ -45,9 +45,27 @@ class Room {
 				+ "\r\n"
 				+ "“Let’s get out of here,” the prisoner says before taking off.\r\n"
 				+ "");
-					//N  S  E  W 
+		//N  S  E  W 
 		r.setExits("", "corridor2","","");
 		roomList.put("startingcell", r);
+		
+		r = new Room("Corridor","You stand in the middle of a long corridor, surrounded by the dungeon cells.");
+		//N  S  E  W 
+		r.setExits("startingcell", "","corridor3","corridor1");
+		roomList.put("corridor2", r);
+		
+		r = new Room("Corridor (West End)", "This side of the corridor leads to the Execution Room. It is surrounded by two other dungeon cells.");
+		//N  S  E  W 
+		r.setExits("", "","corridor2","executionroom");
+		roomList.put("corridor1", r);
+		
+		r = new Room("Execution Room", "This place stinks of death. There are axes on the walls and blood on the ground. You see a brutish man, the Executioner, stand up. \n\n 'Now you die, criminal!' he grunts as he shines his axe.");
+		r.setExits("", "","corridor1","");
+		roomList.put("executionroom", r);
+		
+		r = new Room("Corridor (East End)", "This side of the corridor leads to a giant metal gate, which appears to be lock. There are another two cells surrounding it.");
+		r.setExits("", "","","corridor2");
+		roomList.put("corridor3", r);
 	}
 	
 }
